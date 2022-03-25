1f0736cfffda2fbd491009460488fbc9bef2a910
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-03-26 00:07:51 +0100

    slurm

---
+++ slurm/semcor-training-raw.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --job-name=bert-wsd
+#SBATCH --output=wsd_output.txt
+#SBATCH --ntasks=1
+#SBATCH --mem=32GB
+#SBATCH --gres=gpu:1
+#SBATCH --partition=students
+#SBATCH --nodelist=gpu09
+
+for model in roberta deberta bert-wwm 
+do
+	~/.local/bin/poetry run python main.py modelling \
+		--hf-model ${model} \
+       	--train dataset/${model}+semcor.pickle \
+		--output-path models/${model}+semcor-raw \
+		--epoch 10 \
+        --freeze-lm
+done	
