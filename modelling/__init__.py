93d952180e15a254ddc19234a0e3db1ff7c07413
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-03-26 00:58:43 +0100

    ok maybe it was too much :)

--- modelling/__init__.py
+++ modelling/__init__.py
@@ -198,6 +198,7 @@ def main(**params):
             label_names=["labels", "sense-labels"],
             load_best_model_at_end=True,
             num_train_epochs=params["epoch"],
+            eval_accumulation_steps=200,
             fp16=True,
         )
 
