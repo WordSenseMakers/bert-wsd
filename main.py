3974b07211075afd6a39ece683599870fe6f1a28
tkrieger <lt-krieger@live.de>
2022-03-27 00:18:14 +0100

    Added NLTK downloads to main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import click, colorama
+import nltk
 
 import datagen, modelling, raw
 
 if __name__ == "__main__":
     colorama.init(autoreset=True)
     main = click.Group(commands=[modelling.main, raw.main, datagen.main])
+    nltk.download("wordnet")
+    nltk.download("omw-1.4")
     main()
