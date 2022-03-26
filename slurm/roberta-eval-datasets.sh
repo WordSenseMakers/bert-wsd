bb919e41a65d79b75a0959617a432b44513f544e
Christopher Brückner <christopher.brueckner95@gmail.com>
2022-03-26 23:46:58 +0100

    Slurm scripts

---
+++ slurm/roberta-eval-datasets.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --job-name=roberta-eval-datagen
+#SBATCH --output=datagen.txt
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:0
+#SBATCH --partition=students
+#SBATCH --mem=32GB
+
+for dataset in senseval2 senseval3 semeval2007 semeval2013 semeval2015
+do
+	~/.local/bin/poetry run python main.py datagen \
+		-ds ./test/${dataset}.data.xml \
+		-gs ./test/${dataset}.gold.key.txt \
+		-hm roberta \
+		-op ./dataset/roberta+${dataset}.pickle
+		-te
+done
