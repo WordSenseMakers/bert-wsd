689f284c0cf48b20ac753b8b3f01bbf8ccf48238
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-03-30 23:40:56 +0200

    Remove unused raw functionality

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import click, colorama
 import nltk
 
-import datagen, modelling, raw
+import datagen, modelling
 
 if __name__ == "__main__":
     colorama.init(autoreset=True)
-    main = click.Group(commands=[modelling.main, raw.main, datagen.main])
+    main = click.Group(commands=[datagen.main, modelling.main])
     nltk.download("wordnet")
     nltk.download("omw-1.4")
     main()
